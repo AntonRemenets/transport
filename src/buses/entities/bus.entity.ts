82e37fb806c220cddbec7d33c668df32510d2fba
anton_anton <aremenets1@gmail.com>
2023-12-13 20:17:29 +0500

    мелкие проблемы исправлены

--- src/buses/entities/bus.entity.ts
+++ src/buses/entities/bus.entity.ts
@@ -1,9 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger'
+
 export class Bus {
+  @ApiProperty({ required: false })
   id: number
+
+  @ApiProperty()
   vehicle_number: string
+
+  @ApiProperty()
   vin: string
+
+  @ApiProperty()
   category: string
+
+  @ApiProperty()
   brand: string
+
+  @ApiProperty()
   model: string
+
+  @ApiProperty()
   busRoutesId: number
 }
--- src/drivers/entities/driver.entity.ts
+++ src/drivers/entities/driver.entity.ts
@@ -1,8 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger'
+
 export class Driver {
+  @ApiProperty({ required: false })
   id: number
+
+  @ApiProperty()
   lastName: string
+
+  @ApiProperty()
   firstName: string
+
+  @ApiProperty()
   patronymic?: string
+
+  @ApiProperty()
   license: string
+
+  @ApiProperty()
   busesId: number
 }
